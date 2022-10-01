fcc01d17dc1c1fc8dec58b819ecc5f90186cfbb0
arjun-ingole <arjuningole38@gmail.com>
2022-10-02 00:16:22 +0530

    apiservice: get rid of debug prints and dead code
    Signed-off-by: arjun-ingole <arjuningole38@gmail.com>

--- lib/models/apiservices.dart
+++ lib/models/apiservices.dart
@@ -10,7 +10,6 @@ class ApiService {
       final response = await _dio.get(url);
       var news = response.data['data'] as List;
       List<News> newsList = news.map((m) => News.fromJson(m)).toList();
-      print(newsList[0]);
       return newsList;
     } catch (error, stacktrace) {
       throw Exception(
@@ -18,35 +17,3 @@ class ApiService {
     }
   }
 }
-
-// class Model {
-//   Model({
-//       this.category,
-//       this.data,
-//       this.success,});
-//
-//   Model.fromJson(dynamic json) {
-//     category = json['category'];
-//     if (json['data'] != null) {
-//       data = [];
-//       json['data'].forEach((v) {
-//         data!.add(Data.fromJson(v));
-//       });
-//     }
-//     success = json['success'];
-//   }
-//   String? category;
-//   List<Data>? data;
-//   bool ?success;
-//
-//   Map<String, dynamic> toJson() {
-//     final map = <String, dynamic>{};
-//     map['category'] = category;
-//     if (data != null) {
-//       map['data'] = data!.map((v) => v.toJson()).toList();
-//     }
-//     map['success'] = success;
-//     return map;
-//   }
-//
-// }
