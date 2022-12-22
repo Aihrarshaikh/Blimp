2957263c342b68a9a4ea2e9534ad86fb90191536
aihrar shaikh <aihrarshaikh1208@gmail.com>
2022-12-22 17:14:29 +0530

    Blimp : supabase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:knows/home.dart';
-void main() {
-  runApp(const MyApp());
+import 'package:supabase_flutter/supabase_flutter.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,7 +28,6 @@ class MyApp extends StatelessWidget {
           bodyText2: TextStyle(),
         ).apply(
           bodyColor: Colors.white,
-          // displayColor: Colors.blue,
         ),
       ),
 debugShowCheckedModeBanner:false,
